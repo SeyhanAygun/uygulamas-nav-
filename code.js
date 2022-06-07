0b02cd790ac91ae557f82571b5a07df8030c4f63
SeyhanAygun <96574826+SeyhanAygun@users.noreply.github.com>
2022-06-07 15:06:27 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,11 @@
+let input=document.getElementById("txtsayı")
+const oynama=document.addEventListener("onclick", 1).value;
+const sıfırlama=document.addEventListener("onclick", 2).value;
+let sonuç
+
+Math.floor(Math.random()*100)
+
+function sıfırlama(){
+    document.getElementById("Sıfırla").value
+    sıfırlama=0;
+}
